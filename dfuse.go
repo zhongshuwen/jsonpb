831ad328203796cd91dec2118c72ca631c2a6103
Matthieu Vachon <matt@dfuse.io>
2020-08-19 16:29:48 -0400

    Added MarshalToString helper

--- dfuse.go
+++ dfuse.go
@@ -2,6 +2,11 @@ package jsonpb
 
 import "github.com/golang/protobuf/proto"
 
+func MarshalToString(pb proto.Message) (string, error) {
+	m := &Marshaler{}
+	return m.MarshalToString(pb)
+}
+
 func MarshalIndentToString(pb proto.Message, indent string) (string, error) {
 	m := &Marshaler{
 		Indent: indent,
